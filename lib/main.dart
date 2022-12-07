90f6742784e43ee175ce20bccc436b23735a89b8
mong oh <ohmong0803@gmail.com>
2022-12-07 23:34:11 +0900

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,21 @@ import 'package:learningdart/firebase_options.dart';
 import 'package:learningdart/views/login_view.dart';
 import 'package:learningdart/views/register_view.dart';
 import 'package:learningdart/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-      routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-      },
-    )
-  );
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView(),
+    },
+  ));
 }
 
 class HomePage extends StatelessWidget {
@@ -28,63 +28,124 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder:(context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if(user!=null){
-                if(user.emailVerified){
-                  print('email is verified');
-                } else{
-                  return const VerifyEmailView();
-                }
-              } else{
-                return const LoginView();
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
               }
-              return const Text('Done');
-            default:
-              return const CircularProgressIndicator();
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
         }
-      }, 
+      },
     );
   }
 }
 
+enum MenuAction { logout }
 
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
 
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
 
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogout = await showLogOutDialog(context);
+                if (shouldLogout) {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/login/',
+                    (_) => false,
+                  );
+                }
+                break;
+            }
+          }, itemBuilder: (context) {
+            return [
+              const PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text('Log out'),
+              ),
+            ];
+          })
+        ],
+      ),
+      body: const Text('Hello world'),
+    );
+  }
+}
 
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Sign out'),
+        content: const Text('Are you sure you want to sign out?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out')),
+        ],
+      );
+    },
+  ).then((value) => value ?? false);
+}
 
-
-
-void MA_7to50(){
+void MA_7to50() {
   //Moving Average
   //if 7 days moving average break through 50 days moving average
   // than alert to user!
   //else keep monitoring
 }
-void MA_15to90(){
+void MA_15to90() {
   //Moving Average
   //if 15 days moving average break through 90 days moving average
   // than alert to user!
   //else keep monitoring
 }
-void BB_bt(){
+void BB_bt() {
   //Bollinger Band upper break through
   //if day off price >= upper band && yesterday MACD << today MACD
   //than alert to user!
   //else keep monitoring
 }
-void DI_60(){
+void DI_60() {
   //60 days Disparity Index
   //if disparity index < 90
   //than alert to user!
   //else keep monitoring
 }
-void MT_bull(){
+void MT_bull() {
   //Momentum trading when market is in a bull
   //if All moving average is in a bull
   //than alert to user!
