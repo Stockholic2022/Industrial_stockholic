cb81a2836e8f9a7ebe39e608ff3150d6a26527f7
PARK <pdm7817@naver.com>
2022-12-10 01:40:00 +0900

    login_view_17

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -67,3 +67,6 @@ class _LoginViewState extends State<LoginView> {
           password: password,
         );
         final user = AuthService.firebase().currentUser;
+        if (user?.isEmailVerified ?? false) {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+              notesRoute,
