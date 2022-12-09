44427b802a28681f48b2cf80d40571defe16484b
PARK <pdm7817@naver.com>
2022-12-10 03:01:34 +0900

    auth_service_2

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,3 +1,7 @@
 import 'package:learningdart/services/auth/auth_provider.dart';
 import 'package:learningdart/services/auth/auth_user.dart';
 import 'package:learningdart/services/auth/firebase_auth_provider.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+  const AuthService(this.provider);
