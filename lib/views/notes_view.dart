56941ee621400705e7e0f7da0f85d5a0c7091168
PARK <pdm7817@naver.com>
2022-12-10 02:07:19 +0900

    notes_view_2

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learningdart/constants/routes.dart';
 import 'package:learningdart/enums/menu_action.dart';
+import 'package:learningdart/services/auth/auth_service.dart';
+import 'package:learningdart/services/crud/notes_service.dart';
+
+class NotesView extends StatefulWidget {
