a4aa0e928afeda4b446ae1eac101248fc1ddb9b2
wonchan9999 <wonchan82@gmail.com>
2022-12-10 02:30:38 +0900

    RegisterView 클래스 선언

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -3,3 +3,10 @@ import 'package:learningdart/constants/routes.dart';
 import 'package:learningdart/services/auth/auth_exceptions.dart';
 import 'package:learningdart/services/auth/auth_service.dart';
 import 'package:learningdart/utilities/show_error_dialog.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  _RegisterViewState createState() => _RegisterViewState();
+}
