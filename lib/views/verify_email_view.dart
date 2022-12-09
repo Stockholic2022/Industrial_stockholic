823cee9e76ed7a37683a8c7e98d1972955b15e6f
wonchan9999 <wonchan82@gmail.com>
2022-12-10 02:46:47 +0900

    VerifyEmailView 클래스 선언

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:learningdart/constants/routes.dart';
 import 'package:learningdart/services/auth/auth_service.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  _VerifyEmailViewState createState() => _VerifyEmailViewState();
+}
