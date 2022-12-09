0819d3fc99dec93164c63321852abadbc40402bb
PARK <pdm7817@naver.com>
2022-12-10 03:09:00 +0900

    auth_provider_4

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -18,4 +18,9 @@ class FirebaseAuthProvider implements AuthProvider {
   Future<AuthUser> createUser({
     required String email,
     required String password,
-  }) async
+  }) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
