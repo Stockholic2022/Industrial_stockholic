b7d1c9c696db6f41fa529ace5d2327963580bac7
wonchan9999 <wonchan82@gmail.com>
2022-12-10 02:48:17 +0900

    VerifyEmailViewState 클래스 선언

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -8,3 +8,37 @@ class VerifyEmailView extends StatefulWidget {
   @override
   _VerifyEmailViewState createState() => _VerifyEmailViewState();
 }
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify email'),
+      ),
+      body: Column(
+        children: [
+          const Text(
+              "We've sent you an email verification. Please open it to verify your account."),
+          const Text(
+              "If you haven't received a verification email yet, press the button below"),
+          TextButton(
+            onPressed: () async {
+              await AuthService.firebase().sendEmailVerification();
+            },
+            child: const Text('Send email verification'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await AuthService.firebase().logOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                    (route) => false,
+              );
+            },
+            child: const Text('Restart'),
+          )
+        ],
+      ),
+    );
+  }
+}
