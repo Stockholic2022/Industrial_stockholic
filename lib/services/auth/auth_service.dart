455ada2133994cdf1d7c45aea41c2f4fb644c8ad
PARK <pdm7817@naver.com>
2022-12-10 03:01:42 +0900

    auth_service_3

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -5,3 +5,8 @@ import 'package:learningdart/services/auth/firebase_auth_provider.dart';
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
+  @override
+  Future<AuthUser> createUser({
