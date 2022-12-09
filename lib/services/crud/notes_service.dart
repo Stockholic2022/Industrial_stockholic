3bfaa0db8b9ed6e0015a6692764330f69899eee5
sincheol <ohmong0803@gmail.com>
2022-12-10 02:18:57 +0900

    notes_service59

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -257,3 +257,7 @@ class DatabaseUser {
     required this.id,
     required this.email,
   });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
