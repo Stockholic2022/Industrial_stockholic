51f166a4d935e19e1d28ced17c810e01c6d6acdf
wonchan9999 <wonchan82@gmail.com>
2022-12-10 00:50:15 +0900

    MyApp 클래스 선언

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+
+
+      theme: ThemeData(primarySwatch : Colors.blue,),
+      home: Scaffold(
+        body: Center(
+          child: MyPage(),
+        ),
+      ),
+    );
+  }
+}
