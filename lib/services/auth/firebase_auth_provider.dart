887b6718094cd597aa77dd57e1e4fbd7570adcac
PARK <pdm7817@naver.com>
2022-12-10 03:08:13 +0900

    auth_provider_1

---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:learningdart/firebase_options.dart';
+import 'package:learningdart/services/auth/auth_user.dart';
+import 'package:learningdart/services/auth/auth_provider.dart';
+import 'package:learningdart/services/auth/auth_exceptions.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
